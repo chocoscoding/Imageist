a9a59c46330ddc955163ceb710d3188cab3f9ac3
chocoscoding <chocoscoding@gmail.com>
2024-01-25 12:20:00 -0500

    nextjs top loader

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,24 +1,25 @@
-import MobileNav from '@/components/shared/MobileNav'
-import Sidebar from '@/components/shared/Sidebar'
-import { Toaster } from '@/components/ui/toaster'
+import MobileNav from "@/components/shared/MobileNav";
+import Sidebar from "@/components/shared/Sidebar";
+import { Toaster } from "@/components/ui/toaster";
+import NextTopLoader from "nextjs-toploader";
 
-const Layout = ({ children }: { children: React.ReactNode }) => 
-{
-
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
+      <NextTopLoader
+        color="#5c43ff"
+        showSpinner={false}
+      />
       <Sidebar />
       <MobileNav />
 
       <div className="root-container">
-        <div className="wrapper">
-          {children}
-        </div>
+        <div className="wrapper">{children}</div>
       </div>
-      
+
       <Toaster />
     </main>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
